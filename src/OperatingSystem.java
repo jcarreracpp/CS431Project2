8032d9c3bc08766cc8deb10f39caeb820a6bc636
jcarreracpp <jcarrera@cpp.edu>
2018-02-26 15:31:20 -0800

    First commit, class husks with rough guidelines.

---
+++ src/OperatingSystem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jorge
+ */
+public class OperatingSystem {
+    //Uses the clock algorithm for page replacement.
+    //This will reset the R-bit every 20 instructions.
+}
---
+++ src/VirtualPageTable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jorge
+ */
+public class VirtualPageTable {
+    //THIS WILL BE AN ARRAY OF PAGE TABLE ENTRIES, so one dimensional array.
+    //PAGE OFFSET IS 8 BITS.
+}
