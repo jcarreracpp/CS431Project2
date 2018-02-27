f3f198a407e563765fe1bddcf23f4196da86f6c3
Jacob Kim <kowoon91@gmail.com>
2018-02-26 22:14:51 -0800

    Add files via upload

--- src/TLB.java
+++ src/TLB.java
@@ -10,4 +10,38 @@
  */
 public class TLB {
     //THIS will be a one dimensional array of TLB entries. ONLY 8 ENTRIES.
+    private String vPageNum;
+    private int validBit;
+    private int refBit;
+    private int dirtyBit;
+    private String pageFrameNum;
+    
+    public TLB(){
+        
+    }
+    
+    public boolean lookup(){
+        return false;
+    }
+    
+    public void setVPageNum(String vPageNum){
+        this.vPageNum = vPageNum;
+    }
+    
+    public void setValidBit(int validBit){
+        this.validBit = validBit;
+    }
+    
+    public void setRefBit(int refBit){
+        this.refBit = refBit;
+    }
+    
+    public void setDirtyBit(int dirtyBit){
+        this.dirtyBit = dirtyBit;
+    }
+    
+    public void setPageFrameNum(String pageFrameNum){
+        this.pageFrameNum = pageFrameNum;
+    }
+    
 }
