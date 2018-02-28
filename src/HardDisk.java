4b5fc94c90a35546aa000bcbc2db92eb8aac44c4
jcarreracpp <jcarrera@cpp.edu>
2018-02-27 20:47:58 -0800

    HardDisk class works, stores all page files to avoid volatile editing.

--- src/HardDisk.java
+++ src/HardDisk.java
@@ -1,3 +1,9 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,10 +15,11 @@
  * @author Jorge
  */
 public class HardDisk {
-    private int[][] harddrive = new int[256][256];
-    private HardDisk singleton;
+    private static int[][] harddrive = new int[256][256];
+    private static HardDisk singleton;
+    private static char[] hexGuide = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
     
-    public HardDisk getInstance(){
+    public static HardDisk getInstance() throws FileNotFoundException, IOException{
         if(singleton == null){
             singleton = new HardDisk();
             fillDrive();
@@ -20,7 +27,35 @@ public class HardDisk {
         return singleton;
     }
     
-    private void fillDrive(){
-        
+    private static void fillDrive() throws FileNotFoundException, IOException{
+        for(int i = 0; i < 256; i++){
+            int counter = 0;
+            String pgid = hexify(i);
+            pgid = "page_files\\" + pgid;
+            pgid = pgid.concat(".pg");
+            System.out.println(pgid);
+            BufferedReader br = new BufferedReader(new FileReader(pgid));
+            while((pgid = br.readLine()) != null){
+                harddrive[i][counter] = Integer.parseInt(pgid);
+                counter++;
+            }
+        }
+    }
+    
+    public void display(){
+        for(int i = 0; i < 256; i++){
+            for(int j = 0; j< 256; j++){
+                System.out.println(harddrive[i][j]);
+            }
+        }
+    }
+    
+    private static String hexify(int i){
+        String temp = "";
+        char lead = hexGuide[(i/16)];
+        temp += lead;
+        lead = hexGuide[(i%16)];
+        temp += lead;
+        return temp;
     }
 }
